44330e2544d393c0817678a2b073d5761f72b46e
jshuadvd <jshuadvd@icloud.com>
2016-07-10 20:15:02 -0700

    Adding Object data

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -13,9 +13,9 @@
           description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           posted: '2015-10-24',
           contact: {
-            name: '',
-            phone: '',
-            email: ''
+            name: 'John Doe',
+            phone: '(555)-555-5555',
+            email: 'john@doe.com'
           },
           categories: [
             'Vehicles',
