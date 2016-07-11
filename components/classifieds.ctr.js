705259910ea865d0f25578dce7bbda35738d71c1
jshuadvd <jshuadvd@icloud.com>
2016-07-10 20:10:21 -0700

    Adding Object data

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -11,7 +11,13 @@
           title: 'First Item',
           price: '$100,000.00',
           description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          posted: "2015-10-24",
+          posted: '2015-10-24',
+          contact: {
+            name: '',
+            phone: '',
+            email: ''
+          },
+          
         }
       ]
   });  
