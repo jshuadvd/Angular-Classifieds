055af8582faa6f12f9f01066e9e1f25523c1a992
jshuadvd <jshuadvd@icloud.com>
2016-07-11 22:17:20 -0700

    Adding proper images

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -95,6 +95,24 @@
                   'image': 'http://www.blogcdn.com/www.autoblog.com/media/2012/09/01-2012-tesla-model-s-fd-1347336745.jpg',
                   'views': 213        
                 },
+                {
+                    'id': '6',
+                    'title': 'First Item',
+                    'price': 800,
+                    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    'posted': '2015-10-24',
+                    'contact': {
+                      name: 'John Doe',
+                      phone: '(555)-555-5555',
+                      email: 'john@doe.com'
+                    },
+                    'categories': [
+                      'Vehicles',
+                      'Parts & Accesories' 
+                    ],
+                    'image': 'http://www.blogcdn.com/www.autoblog.com/media/2012/09/01-2012-tesla-model-s-fd-1347336745.jpg',
+                    'views': 213        
+                  }
       ]
   });  
 })();
