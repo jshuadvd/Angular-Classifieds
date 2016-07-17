26183b40ccab2a946dbc42270d45363b4cc9e28d
jshuadvd <jshuadvd@icloud.com>
2016-07-16 17:52:54 -0700

    Moving data out

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -5,7 +5,8 @@
     angular
         .module('ngClassifieds')
         .controller('classifiedsCtrl', function($scope, $http) {
-          $http.get('data/classifieds.json');
+          $http.get('data/classifieds.json').then(function(data) {
             $scope.classifieds = 
+          })
         });
 })();
