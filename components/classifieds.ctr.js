d91912bf4fe4b7430fce97cb727d2dde6800c630
jshuadvd <jshuadvd@icloud.com>
2016-07-21 23:52:44 -0700

    Adding sidebar

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -6,9 +6,9 @@
         .module('ngClassifieds')
         .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory) {
           console.log(classifiedsFactory.getClassifieds);
-          classifiedsFactory.getClassifieds()
-          .then(function(response) {
-            $scope.classifieds = response.data;
-          })
+          // classifiedsFactory.getClassifieds()
+          // .then(function(response) {
+          //   $scope.classifieds = response.data;
+          // })
         });
 })();
