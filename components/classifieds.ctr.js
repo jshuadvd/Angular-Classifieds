4853ffa970368502b420f2dd9299b179ba28f06a
jshuadvd <jshuadvd@icloud.com>
2016-07-23 23:22:24 -0700

    Getting data working

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,8 +4,8 @@
         .module('ngClassifieds')
         .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory) {
           // why undefined
-          // classifiedsFactory.getClassifieds().then(function(response) {
-          //   $scope.classifieds = response.data;
-          // })
+          classifiedsFactory.getClassifieds().then(function(response) {
+            $scope.classifieds = response.data;
+          })
         });
 })();
