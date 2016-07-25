51107f559790f5629c2aad034db8c45ceadbb038
jshuadvd <jshuadvd@icloud.com>
2016-07-24 23:53:04 -0700

    Trouble shooting errors

--- components/classifieds.fac.js
+++ components/classifieds.fac.js
@@ -4,9 +4,7 @@
   angular
   .module("ngClassifieds")
   .factory("classifiedsFactory", function($http) {
-    
-    // Trouble shooting
-    
+        
     function getClassifieds() {
       $http.get('data/classifieds.json');
     }
