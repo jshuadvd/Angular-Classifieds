ca6b8fb3b24bad96992e2c90f99d0249073c177a
jshuadvd <jshuadvd@icloud.com>
2016-07-25 21:13:59 -0700

    Remove comment

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,7 +3,6 @@
     angular
         .module('ngClassifieds')
         .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory) {
-          // why is this undefined
           classifiedsFactory.getClassifieds()
           .then(function(response) {
             console.log($scope.classifieds);
