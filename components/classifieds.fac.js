9d1a297647b8948e8ec0ac3a45c8d12d2792014c
jshuadvd <jshuadvd@icloud.com>
2016-07-29 23:43:28 -0700

    Comments

--- components/classifieds.fac.js
+++ components/classifieds.fac.js
@@ -5,6 +5,7 @@
   .module("ngClassifieds")
   .factory("classifiedsFactory", function($http) {
         
+    // Get Classified    
     function getClassifieds() {
       return $http.get('data/classifieds.json');
     }
