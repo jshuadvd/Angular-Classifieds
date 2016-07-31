928e92d404c1136538dd86942d915f0349ed4ce2
jshuadvd <jshuadvd@icloud.com>
2016-07-30 23:34:35 -0700

    Test it

--- components/classifieds.fac.js
+++ components/classifieds.fac.js
@@ -5,7 +5,6 @@
   .module("ngClassifieds")
   .factory("classifiedsFactory", function($http) {
         
-    // Get Classified  Method  
     function getClassifieds() {
       return $http.get('data/classifieds.json');
     }
