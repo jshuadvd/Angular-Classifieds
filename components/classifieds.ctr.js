b71e16b7d0215a79a1a9a5eaf1fef29ca1d53012
jshuadvd <jshuadvd@icloud.com>
2016-08-03 23:32:36 -0700

    working sidebar

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,7 +4,7 @@
     
     angular
         .module('ngClassifieds')
-        .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory) {
+        .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory, $mdSidenav) {
           classifiedsFactory.getClassifieds()
           .then(function(response) {
             $scope.classifieds = response.data;
