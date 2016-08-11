19713b2f25d6c0fc9c4a583b453ae29b1dbc3008
jshuadvd <jshuadvd@icloud.com>
2016-08-10 23:48:00 -0700

    Scope updates

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -5,6 +5,7 @@
     angular
         .module('ngClassifieds')
         .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory, $mdSidenav) {
+            
           classifiedsFactory.getClassifieds()
           .then(function(response) {
             $scope.classifieds = response.data;
