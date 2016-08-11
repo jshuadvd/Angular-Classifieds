74f5c6e61290f1eac470b2ca4b81b60477603f66
jshuadvd <jshuadvd@icloud.com>
2016-08-10 23:49:00 -0700

    Scope updates

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -1,32 +1,34 @@
 (function() {
-  
+    
     "use strict";
     
     angular
-        .module('ngClassifieds')
-        .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory, $mdSidenav) {
-            
-          classifiedsFactory.getClassifieds()
-          .then(function(response) {
+    .module('ngClassifieds')
+    .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory, $mdSidenav) {
+        
+        classifiedsFactory.getClassifieds()
+        .then(function(response) {
             $scope.classifieds = response.data;
-          });
-                  
-          $scope.message = "Hello!";
-          
-          $scope.openSideBar = function() {
-            $mdSidenav('left').open();
-          }
-          
-          $scope.closeSideBar = function() {
-            $mdSidenav('left').close();
-          }
-          
-          $scope.saveClassified = function(classified) {
-              if (classified) {
-                  $scope.classifieds.push(classified);
-                  $scope.classified = {};
-                  $scope.closeSideBar();
-              }
-          }
         });
+        
+        var contact;
+        
+        $scope.message = "Hello!";
+        
+        $scope.openSideBar = function() {
+            $mdSidenav('left').open();
+        }
+        
+        $scope.closeSideBar = function() {
+            $mdSidenav('left').close();
+        }
+        
+        $scope.saveClassified = function(classified) {
+            if (classified) {
+                $scope.classifieds.push(classified);
+                $scope.classified = {};
+                $scope.closeSideBar();
+            }
+        }
+    });
 })();
