127b07bb9dfe3f1bb0c7896b15c3db91a22a8d4e
jshuadvd <jshuadvd@icloud.com>
2016-10-18 20:31:43 -0700

    Binding this to te meassage

--- scripts/app.js
+++ scripts/app.js
@@ -12,14 +12,7 @@ angular
         template: '<h1>{{ stateone.message }}</h1>',
         controller: 'stateOneCtrl as stateone'
     })    
-    .state('two', {
-        url: '/statetwo',
-        template: '<h1>State Two</h1> <md-button ui-sref="two.more">Go to nested state.</md-button> <ui-view></ui-view>' 
-    })
-    .state('two.more', {
-        url: '/more'
-        template: '<p>This is the deeper state</p>'
-    });
+    
     
   })
   .directive('helloWorld', function() {
